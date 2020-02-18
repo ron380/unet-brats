1652916cdf021a81b313ee842c921f22007b43e6
Omar Andres Zapata Mesa <omar.zapata@cern.ch>
2020-02-18 13:16:40 +0100

    added code for trainin and inference, and git submodules for the required repositories

---
+++ deps.sh
@@ -0,0 +1,3 @@
+pip install opencv-python nibabel imutils SimpleITK nipype -t deps
+pip install -r requirements.txt -t deps/
+pip install git+https://www.github.com/keras-team/keras-contrib.git -t deps/
---
+++ preprocess.py
@@ -0,0 +1,2 @@
+from brats.preprocess import convert_brats_data 
+convert_brats_data("brats/data/original", "brats/data/preprocessed")
